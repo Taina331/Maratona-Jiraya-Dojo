ebee2688270f79ff5959a5037b1e49216dd150d4
Taina331 <tainabmoraes@gmail.com>
2021-07-28 12:22:16 -0300

    Projetos do curso maratona java

---
+++ src/com/dojo/projeto01/Aula01.java
@@ -0,0 +1,16 @@
+package com.dojo.projeto01;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+        int idade = 10;
+        long numeroGrande = 100000l;
+        double salarioDouble = 2000.0d;
+        float salarioFloat = 2500.0f;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
+        boolean falseTrue = false;
+        char caractere = 'M';
+
+    }
+}
---
+++ src/com/dojo/projeto02/Aula02.java
@@ -0,0 +1,20 @@
+package com.dojo.projeto02;
+
+import java.text.DecimalFormat;
+import java.util.Date;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        String nome = "Maria";
+        String endereco = "rua dos poetas";
+        double salario = 2000;
+        String dataRecebida = "20/12/2021";
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println("Eu " + nome );
+        System.out.println(" morando no endereço: " + endereco );
+        System.out.println(" Confirmo que recebir o salario de: " + df.format(salario));
+        System.out.println(" Na data:" + dataRecebida);
+    }
+}
